9bf1522c758158db41ba777ab400861dffd70553
DarknessShadow <DarknessShadow@web.de>
2016-05-04 20:40:10 +0200

    Update autorun.lua

--- autorun.lua
+++ autorun.lua
@@ -1,3 +1,3 @@
 os.execute("wget -f 'https://raw.githubusercontent.com/DarknessShadow/Wither-killing-programm/master/autorun.lua' autorun.lua")
 os.execute("wget -f 'https://raw.githubusercontent.com/DarknessShadow/Wither-killing-programm/master/wither.lua' wither.lua")
---dofile("wither.lua")
+dofile("wither.lua")
