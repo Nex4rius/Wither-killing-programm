508771d37a7d112aba7f7bd0351f7899b03ca1c9
DarknessShadow <DarknessShadow@web.de>
2016-05-04 16:30:10 +0200

    Create autorun.lua

---
+++ autorun.lua
@@ -0,0 +1,4 @@
+os.execute("wget -f 'https://raw.githubusercontent.com/DarknessShadow/Wither-killing-programm/master/autorun.lua' autorun.lua")
+print("\n")
+os.execute("wget -f 'https://raw.githubusercontent.com/DarknessShadow/Wither-killing-programm/master/wither.lua' wither.lua")
+dofile("wither.lua")
