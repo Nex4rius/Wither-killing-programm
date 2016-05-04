794d617d2ced12d8033cae6ef2c429624027763b
DarknessShadow <DarknessShadow@web.de>
2016-05-04 16:54:24 +0200

    Update wither.lua

--- wither.lua
+++ wither.lua
@@ -1,6 +1,10 @@
 local component = require("component")
 r = require("robot")
 
+WitherSkeletonSkull = 0
+SoulSand = 0
+WardedGlass = 0
+
 function enter()
   r.turnRight()
   r.forward()
@@ -30,7 +34,7 @@ end
 
 function placeWither()
   checkInventory()
-  print(WitherSkeletonSkull .. SoulSand .. WardedGlass)
+  print(WitherSkeletonSkull .. "   " .. SoulSand .. "   " .. WardedGlass)
 end
 
 function checkInventory()
@@ -38,13 +42,13 @@ function checkInventory()
     checkSlot(i)
     if item then
       name = item.name .. item.damage
-      if minecraft:skull1 == name and 3 <= item.size then
+      if "minecraft:skull1" == name and 3 <= item.size then
         WitherSkeletonSkull = i
       end
-      if minecraft:soul:sand0 == name and 4 <= item.size then
+      if "minecraft:soul:sand0" == name and 4 <= item.size then
         SoulSand = i
       end
-      if Thaumcraft:blockCosmeticOpaque2 == name then
+      if "Thaumcraft:blockCosmeticOpaque2" == name then
         WardedGlass = i
       end
     end
