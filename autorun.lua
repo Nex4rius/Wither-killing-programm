6ed29c6bbece5a907e24db086dfbae85cac3901c
DarknessShadow <DarknessShadow@web.de>
2016-05-07 11:13:52 +0200

    Update autorun.lua

--- autorun.lua
+++ autorun.lua
@@ -1,7 +1,14 @@
-os.execute("wget -f 'https://raw.githubusercontent.com/DarknessShadow/Wither-killing-programm/master/autorun.lua' autorun.lua")
-print("")
-os.execute("wget -f 'https://raw.githubusercontent.com/DarknessShadow/Wither-killing-programm/master/wither.lua' wither.lua")
-print("")
-os.execute("wget 'https://raw.githubusercontent.com/DarknessShadow/Wither-killing-programm/master/saveAfterReboot.lua' saveAfterReboot.lua")
-os.execute("clear")
+component = require("component")
+if component.isAvailable("internet") then
+  print("- Internet Card        ok (optional)")
+  print("")
+  os.execute("wget -f 'https://raw.githubusercontent.com/DarknessShadow/Wither-killing-programm/master/autorun.lua' autorun.lua")
+  print("")
+  os.execute("wget -f 'https://raw.githubusercontent.com/DarknessShadow/Wither-killing-programm/master/wither.lua' wither.lua")
+  print("")
+  os.execute("wget 'https://raw.githubusercontent.com/DarknessShadow/Wither-killing-programm/master/saveAfterReboot.lua' saveAfterReboot.lua")
+  os.execute("clear")
+else
+  print("- Internet Card        Missing (optional)")
+end
 dofile("wither.lua")
