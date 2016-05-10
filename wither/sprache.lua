07d3cbd6d65846fadd3e509e0117f06501c45ca4
DarknessShadow <DarknessShadow@web.de>
2016-05-10 14:50:28 +0200

    Create sprache.lua

---
+++ wither/sprache.lua
@@ -0,0 +1,19 @@
+if sprache == "english" then
+  chunkloaderOK             = "- ChunkLoader          ok (optional)"
+  chunkloaderFehlt          = "- ChunkLoader          missing (optional)"
+  generatorOK               = "- Generator            ok (optional)"
+  generatorFehlt            = "- Generator            missing (optional)"
+  InternetOK                = "- Internet             ok (optional)"
+  InternetFehlt             = "- Internet             missing (optional)"
+  inventory_controllerOK    = "- Inventory Controller ok\n"
+  inventory_controllerFehlt = "- Inventory Controller missing\n"
+else
+  chunkloaderOK             = "- ChunkLoader          ok (optional)"
+  chunkloaderFehlt          = "- ChunkLoader          fehlt (optional)"
+  generatorOK               = "- Generator            ok (optional)"
+  generatorFehlt            = "- Generator            fehlt (optional)"
+  InternetOK                = "- Internet             ok (optional)"
+  InternetFehlt             = "- Internet             fehlt (optional)"
+  inventory_controllerOK    = "- Inventory Controller ok\n"
+  inventory_controllerFehlt = "- Inventory Controller fehlt\n"
+end
